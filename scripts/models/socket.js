038ef5bb752659250270183fc2806330f616b8e9
Omran Jamal <o.jamal97@gmail.com>
2015-12-08 19:16:13 +0600

    Basic Socket Model Done

---
+++ scripts/models/socket.js
@@ -0,0 +1,43 @@
+define('models/socket', [
+	'backbone'
+], function (Backbone) {
+	var SocketModel = Backbone.Model.extend({
+		defaults: {
+			host: window.location.host,
+			port: 80,
+			path: '',
+			secure: window.location.protocol === 'https'
+		},
+		socket: null,
+		url: function () {
+			return 'ws' + 
+				(this.get('secure') === true ? 's' : '') + // If Secure connection
+				'://' + this.get('host') +
+				':' + this.get('port') + 
+				'/' + this.get('path');
+		},
+		initialize: function () {
+			// Register the Socket
+			var socket = this.socket = new WebSocket(this.url());
+
+			// Convert Native Socket Events to Model Events
+			socket.addEventListener('open', function (event) {
+				this.trigger('open', event);
+			}.bind(this));
+
+			socket.addEventListener('error', function (event) {
+				this.trigger('error', event);
+			}.bind(this));
+
+			socket.addEventListener('message', function (event) {
+				this.trigger('message', event);
+			}.bind(this));
+
+			socket.addEventListener('close', function (event) {
+				this.trigger('close', event);
+			}.bind(this));
+		}
+	});
+
+	return SocketModel;
+});
